10caf893532ca9927487e071517c72578584ad91
Abhinay Ahirwar <abhinayahirwar788@gmail.com>
2026-06-13 22:06:34 +0530

    new cross origin

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,10 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors())
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
